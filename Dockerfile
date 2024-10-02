50b5871aac88ba6541b5abc7768b085fedaba227
holden <asparagusfern27@outlook.com>
2024-10-02 22:59:29 +0800

    fix some bugs

--- Dockerfile
+++ Dockerfile
@@ -4,20 +4,19 @@ ENV DEBIAN_FRONTEND=noninteractive
 WORKDIR /iNode
 COPY iNodeClient.tar.gz /iNode
 COPY run.sh /iNode
-COPY service_startup.sh /iNode
-COPY restartUI.sh /iNode
-RUN chmod 755 restartUI.sh \
-&& chmod 755 service_startup.sh \
-&& chmod 755 /iNode/run.sh \
+RUN chmod 755 /iNode/run.sh \
 && apt update \
 && apt upgrade -y\
 && apt install -y libgtk2.0-0 libqt5widgets5 libsm6 libxext6 libxrender-dev \
 && apt install -y xvfb\
 && apt install -y x11vnc\
+&& apt install krb5-user\
 && tar -xzf iNodeClient.tar.gz \
 && rm iNodeClient.tar.gz \
 && cd iNodeClient\
 && ./install_64.sh
+
+CMD /iNode/run.sh
 
 
 EXPOSE 8080
--- run.sh
+++ run.sh
@@ -1,29 +1,79 @@
+#!/bin/bash
+
+# define the PID file
+PID_FILE="./pid.log"
+
+stop_services(){
+        echo "stopping services"
+
+        if [ ! -f $PID_FILE ];then
+                echo "Can't find pid.log file in $PID_FILE"
+                exit 1
+        fi
+
+        while IFS=: read -r service pid;do
+                echo "stopping $service PID:$pid"
+                kill -9 $pid
+                if [ $? -eq 0 ];then
+                        echo "$service stopped"
+                else
+                        kill -9 `pidof $service`
+                        if [ $? -eq 0 ];then
+                                echo "$service stopped"
+                        else
+                                echo "$service stopped failed"
+                        fi
+                fi
+        done < $PID_FILE
+}
+
+
+stop_services
+echo "Starting"
+
+ > $PID_FILE
+
+# start Xvfb
 Xvfb :0 -screen 0 1360x720x24 -listen tcp -ac +extension GLX +extension RENDER > /iNode/xvfb.log 2>&1 &\
+XVFB=$!
+echo "Xvfb : $XVFB"
+
+# start vnc service
+
 x11vnc -forever -shared -noipv6 -passwd wenzhu27 > /iNode/x11vnc.log 2>&1 &\
-# 启动服务
-# start service
-echo -n "Starting AuthenMngService: "
+X11VNC=$!
+echo "x11vnc : $X11VNC"
+
+
+# start inode services , create some necessary files
+
 IfExist=`ps awx -o command|awk -F/ '{print $NF}'|grep -x AuthenMngService`
     if [ "$IfExist" != "" ]
     then
         echo "AuthenMngService already running"
     else
-        nohup "/iNode/iNodeClient/AuthenMngService" > /dev/null 2>&1 &
-        echo OK
+        "/iNode/iNodeClient/AuthenMngService" > /dev/null 2>&1 &
+        AMS=`pidof AuthenMngService`
+        echo "AuthenMngService : $AMS"
     fi
+
 
 IfExist=`ps awx -o command|awk -F/ '{print $NF}'|grep -x iNodeMon`
     if [ "$IfExist" = "" ]
     then
         sleep 5
-        nohup "/iNode/iNodeClient/iNodeMon" > /dev/null 2>&1 &
-        echo "iNodeMon is started"
+        "/iNode/iNodeClient/iNodeMon" > /dev/null 2>&1 &
+        INM=`pidof iNodeMon`
+        echo "iNodeMon : $INM"
     fi
 
     touch /var/lock/subsys/iNodeAuthService
     touch /var/run/AuthenMngService.pid
+
 getPid=`ps |grep AuthenMng|awk '{print $1}'`
-# 每秒尝试执行一次，总共50次
+
+sleep 5
+
 for i in `seq 50`
 do
         IfExistUI=`pidof iNodeClient`
@@ -31,9 +81,17 @@ do
         echo "$i curPid = $curPid; getPid = $getPid " > "/iNode/iNodeClient/getAuthPid.txt"
         if [ "$curPid" != "" -a "$IfExistUI" = "" ]
         then
-        DISPLAY=:0.0 nohup "/iNode/iNodeClient/.iNode/iNodeClient" &
+                DISPLAY=:0.0 "/iNode/iNodeClient/.iNode/iNodeClient" &
+                INC=`pidof iNodeClient`
+                echo "iNodeClient : $INC"
                 break
         else
                 sleep 1
         fi
 done
+echo "iNodeClient : $INC" >> $PID_FILE
+echo "iNodeMon : $INM" >> $PID_FILE
+echo "AuthenMngService : $AMS" >> $PID_FILE
+echo "x11vnc : $X11VNC" >> $PID_FILE
+echo "Xvfb : $XVFB" >> $PID_FILE
+tail -f /dev/null
